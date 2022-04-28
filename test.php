3a6a2c14d045fcba274814d732ddf009ef99e814
Richard <accdou@163.com>
2022-04-28 13:29:16 +0800

    ..

--- test.php
+++ test.php
@@ -32,5 +32,30 @@ if ($conn->connect_error) {
 echo "连接成功";
 ?>
 
+<?php
+$servername = "localhost";
+$username = "sql_tk_pyabc_net";
+$password = "6cT5kSkd24WePTPz";
+$dbname = "sql_tk_pyabc_net";
+ 
+// 创建连接
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// 检测连接
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+ 
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+ 
+if (mysqli_query($conn, $sql)) {
+    echo "新记录插入成功";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+ 
+mysqli_close($conn);
+?>
+
 </body>
 </html>
