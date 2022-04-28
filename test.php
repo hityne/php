1ace36c199524efe9951715ffe1098eb53bb2102
Richard <accdou@163.com>
2022-04-28 13:23:43 +0800

    ..

--- test.php
+++ test.php
@@ -16,6 +16,21 @@ if ($t<"20")
     echo "Have a good day!";
 }
 ?>
+<br>
+<?php
+$servername = "localhost";
+$username = "sql_tk_pyabc_net";
+$password = "6cT5kSkd24WePTPz";
+ 
+// 创建连接
+$conn = new mysqli($servername, $username, $password);
+ 
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+echo "连接成功";
+?>
 
 </body>
 </html>
